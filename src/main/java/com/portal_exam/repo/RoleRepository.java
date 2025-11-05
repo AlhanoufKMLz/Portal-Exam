31034c7be751336bfbaf62a17b303a0f8fd719ef
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2025-11-05 23:24:18 +0300

    Delete duplicated files

--- src/main/java/com/portal_exam/repo/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portal_exam.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.portal_exam.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-
-}
